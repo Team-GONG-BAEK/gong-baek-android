c04328f6e234fff068607d55a2339496d4fc005b
wjdrjs00 <kimjk3409@gmail.com>
2025-03-19 20:58:49 +0900

    [chore] import문 제거

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/main/MainNavigator.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/main/MainNavigator.kt
@@ -9,7 +9,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
-import com.sopt.gongbaek.presentation.model.MainBottomTabRoute
 import com.sopt.gongbaek.presentation.model.NavigationRoute
 import com.sopt.gongbaek.presentation.type.MainBottomNavBarTabType
 import com.sopt.gongbaek.presentation.ui.grouplist.navigation.navigateGroupListNavGraph
