e426f74ad4c82f27aa20c11ff81c47bf09555030
wjdrjs00 <kimjk3409@gmail.com>
2025-03-19 20:13:25 +0900

    [refactor] MainBottomTabRoute 인터페이스 정의
    - MainBottomNavBarTabRoute 객체 -> MainBottomTabRoute 인터페이스로 변경

--- app/src/main/java/com/sopt/gongbaek/presentation/model/NavigationRoute.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/model/NavigationRoute.kt
@@ -6,13 +6,7 @@ object NavigationRoute {
     const val GROUP_DETAIL = "group_detail/{groupId}/{groupCycle}"
     const val GROUP_ROOM = "group_room/{groupId}/{groupCycle}"
 
-    object MainBottomNavBarTabRoute {
-        const val GROUP_LIST_TAB = "group_list_route"
-        const val HOME_TAB = "home_route"
 
-//        const val TIMETABLE_TAB = "timetable_route"
-        const val MY_PAGE_TAB = "my_page_route"
-    }
 
 
 }
