fc7b9d5a18f244c7e4b6af48b639bfbe00ba7ce0
wjdrjs00 <kimjk3409@gmail.com>
2025-03-19 16:10:19 +0900

    [chore] import문 제거

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/home/navigation/HomeNavGraph.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/home/navigation/HomeNavGraph.kt
@@ -3,7 +3,6 @@ package com.sopt.gongbaek.presentation.ui.home.navigation
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
 import com.sopt.gongbaek.presentation.model.NavigationRoute
 import com.sopt.gongbaek.presentation.ui.groupdetail.navigation.navigateGroupDetail
 import com.sopt.gongbaek.presentation.ui.grouplist.navigation.navigateGroupList
