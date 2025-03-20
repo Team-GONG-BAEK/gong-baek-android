488cb355e10cb75df071eaccda3e0740797f87c9
SYAAINN <alswo08319@gmail.com>
2025-03-20 16:13:40 +0900

    [chore] 스플래시 후 로그인 화면으로 이동하도록 수정합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/splash/SplashScreen.kt
@@ -28,7 +28,7 @@ fun SplashScreen(
     val backgroundColor = GongBaekTheme.colors.gray10
     val defaultBackgroundColor = GongBaekTheme.colors.white
     val onComplete = {
-        navController.navigate(NavigationRoute.OnboardingNavGraph.ONBOARDING_NAV_GRAPH) {
+        navController.navigate(NavigationRoute.SIGN_IN) {
             popUpTo(NavigationRoute.SplashRoute.SPLASH) { inclusive = true }
         }
     }
