195ad22e8fc19ab25f5497da852a4b0e26aa653d
MinseoSONG <mins041600@naver.com>
2025-03-26 00:01:35 +0900

    [feat] topbar 구현

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/mypage/screen/MyPageScreen.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/mypage/screen/MyPageScreen.kt
@@ -1,12 +1,19 @@
 package com.sopt.gongbaek.presentation.ui.mypage.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import com.sopt.gongbaek.R
+import com.sopt.gongbaek.presentation.ui.component.topbar.CenterTitleTopBar
+import com.sopt.gongbaek.ui.theme.GONGBAEKTheme
+import com.sopt.gongbaek.ui.theme.GongBaekTheme
 
 @Composable
 fun MyPageRoute() {
@@ -16,17 +23,26 @@ fun MyPageRoute() {
 @Composable
 fun MyPageScreen() {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 10.dp)
+            .padding(WindowInsets.navigationBars.asPaddingValues())
     ) {
-        Text(
-            text = "여긴 마이페이지",
-            fontSize = 30.sp
+        CenterTitleTopBar(
+            centerTitleResId = R.string.topbar_my_page,
+            trailingIconResId = R.drawable.ic_setting_48,
+            onTrailingIconClick = {},
+            trailingIconColor = GongBaekTheme.colors.gray08
         )
+
+
     }
 }
 
 @Preview
 @Composable
 private fun MyPageScreenPreview() {
-    MyPageScreen()
+    GONGBAEKTheme {
+        MyPageScreen()
+    }
 }
