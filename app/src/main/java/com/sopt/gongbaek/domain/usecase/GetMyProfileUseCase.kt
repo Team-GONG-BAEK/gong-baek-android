0d9e68869c7e4966f75430a27adb53d058f7d3f2
MinseoSONG <mins041600@naver.com>
2025-03-31 01:06:32 +0900

    [feat] GetMyProfileUseCase 구현

---
+++ app/src/main/java/com/sopt/gongbaek/domain/usecase/GetMyProfileUseCase.kt
@@ -0,0 +1,10 @@
+package com.sopt.gongbaek.domain.usecase
+
+import com.sopt.gongbaek.domain.model.UserInfo
+import com.sopt.gongbaek.domain.repository.AuthRepository
+
+class GetMyProfileUseCase(
+    private val authRepository: AuthRepository
+){
+    suspend operator fun invoke(): Result<UserInfo> = authRepository.getMyProfile()
+}
