dfc36105a5d609c4683fe367d44ecf85c9919846
wjdrjs00 <kimjk3409@gmail.com>
2025-04-09 16:25:22 +0900

    [feat] mbti, 성별 타입 명확성을 위한 변환 메소드 정의

--- app/src/main/java/com/sopt/gongbaek/domain/type/MbtiFourthLetterType.kt
+++ app/src/main/java/com/sopt/gongbaek/domain/type/MbtiFourthLetterType.kt
@@ -4,5 +4,14 @@ enum class MbtiFourthLetterType(
     val description: String
 ) {
     P(description = "P"),
-    J(description = "J")
+    J(description = "J");
+
+    companion object {
+        private const val INVALID_DESCRIPTION_ERROR_MESSAGE = "유효하지 않은 MBTI 타입입니다: "
+
+        fun fromDescription(description: String): MbtiFourthLetterType =
+            requireNotNull(entries.find { it.description == description }) {
+                INVALID_DESCRIPTION_ERROR_MESSAGE + description
+            }
+    }
 }
--- app/src/main/java/com/sopt/gongbaek/domain/type/MbtiSecondLetterType.kt
+++ app/src/main/java/com/sopt/gongbaek/domain/type/MbtiSecondLetterType.kt
@@ -4,5 +4,14 @@ enum class MbtiSecondLetterType(
     val description: String
 ) {
     N(description = "N"),
-    S(description = "S")
+    S(description = "S");
+
+    companion object {
+        private const val INVALID_DESCRIPTION_ERROR_MESSAGE = "유효하지 않은 MBTI 타입입니다: "
+
+        fun fromDescription(description: String): MbtiSecondLetterType =
+            requireNotNull(entries.find { it.description == description }) {
+                INVALID_DESCRIPTION_ERROR_MESSAGE + description
+            }
+    }
 }
