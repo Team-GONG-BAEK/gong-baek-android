1ff2b7cad3ec9471087729ca522a17c647469b7f
wjdrjs00 <kimjk3409@gmail.com>
2025-04-13 02:02:35 +0900

    [feat] 추천맴버 커밍순 이미지로 대체

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/home/component/section/MemberRecommendSection.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/home/component/section/MemberRecommendSection.kt
@@ -1,33 +1,28 @@
 package com.sopt.gongbaek.presentation.ui.home.component.section
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.gongbaek.R
-import com.sopt.gongbaek.ui.theme.GONGBAEKTheme
 import com.sopt.gongbaek.ui.theme.GongBaekTheme
 
 @Composable
-fun MemberRecommendSection() {
+fun MemberRecommendSection(
+    modifier: Modifier = Modifier
+) {
     Column(
-        Modifier.padding(horizontal = 16.dp)
+        modifier = modifier.padding(horizontal = 16.dp)
     ) {
         Text(
             text = "나와 딱 맞는 공백 멤버",
@@ -39,114 +34,25 @@ fun MemberRecommendSection() {
 
         Text(
             text = "공백 시간에 이 멤버들과 공백 활동 어때요?",
-            color = GongBaekTheme.colors.gray10,
+            color = GongBaekTheme.colors.gray06,
             style = GongBaekTheme.typography.body2.m14
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Column {
-            RecommendOtherMemberItem(
-                userNickname = "로이임탄",
-                profileImage = R.drawable.img_home_recommend_2
-            )
-            RecommendOtherMemberItem(
-                userNickname = "누구게",
-                profileImage = R.drawable.img_home_recommend_6
-            )
-            RecommendOtherMemberItem(
-                userNickname = "누굴까",
-                profileImage = R.drawable.img_home_recommend_4
-            )
-        }
-    }
-}
-
-@Composable
-private fun RecommendOtherMemberItem(
-    userNickname: String,
-    profileImage: Int
-) {
-    Box(
-        modifier = Modifier
-            .padding(vertical = 10.dp)
-            .fillMaxWidth()
-
-    ) {
-        Row(
-            modifier = Modifier.align(Alignment.CenterStart),
-            horizontalArrangement = Arrangement.spacedBy(10.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = painterResource(profileImage),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-            )
-
-            Column(
-                modifier = Modifier.padding(vertical = 3.dp),
-                verticalArrangement = Arrangement.spacedBy(3.dp)
-            ) {
-                Text(
-                    text = userNickname,
-                    color = GongBaekTheme.colors.gray10,
-                    style = GongBaekTheme.typography.body2.sb14
-                )
-                Box(
-                    modifier = Modifier
-                        .background(
-                            color = GongBaekTheme.colors.subOrange,
-                            shape = RoundedCornerShape(4.dp)
-                        )
-                        .padding(horizontal = 6.dp, vertical = 1.dp)
-                ) {
-                    Text(
-                        text = "컴퓨터예술학부",
-                        color = GongBaekTheme.colors.mainOrange,
-                        style = GongBaekTheme.typography.caption2.m12
-                    )
-                }
-            }
-        }
-
-        Column(
+        Image(
+            painter = painterResource(R.drawable.img_home_comingsoon),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
-                .background(
-                    color = GongBaekTheme.colors.mainOrange,
-                    shape = RoundedCornerShape(4.dp)
-                )
-                .align(Alignment.CenterEnd),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "채팅하기",
-                color = GongBaekTheme.colors.white,
-                style = GongBaekTheme.typography.caption2.b12,
-                modifier = Modifier.padding(horizontal = 10.dp, vertical = 4.dp)
-            )
-        }
-    }
-    HorizontalDivider(
-        thickness = 1.dp,
-        color = GongBaekTheme.colors.gray02
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun PreviewMemberRecommendSection() {
-    MemberRecommendSection()
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun PreviewOtherMemberItem() {
-    GONGBAEKTheme {
-        RecommendOtherMemberItem(
-            userNickname = "로이임탄",
-            profileImage = R.drawable.img_home_recommend_2
+                .fillMaxWidth()
+                .aspectRatio(328f / 204f)
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MemberRecommendSectionPreview() {
+    MemberRecommendSection()
 }
