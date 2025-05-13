e0cd89144955e3fd270c88d0873d12b09feef7bb
SYAAINN <alswo08319@gmail.com>
2025-05-14 00:15:31 +0900

    [refactor] 직렬화/역직렬화 방식을 Serialization 라이브러리를 이용한 방식으로 통일합니다.

--- app/src/main/java/com/sopt/gongbaek/data/remote/util/HttpResponseHandler.kt
+++ app/src/main/java/com/sopt/gongbaek/data/remote/util/HttpResponseHandler.kt
@@ -1,8 +1,8 @@
 package com.sopt.gongbaek.data.remote.util
 
-import com.google.gson.Gson
 import com.sopt.gongbaek.data.remote.dto.base.ApiResponse
 import com.sopt.gongbaek.data.remote.dto.base.NullableApiResponse
+import kotlinx.serialization.json.Json
 import retrofit2.HttpException
 
 object HttpResponseHandler {
@@ -23,8 +23,10 @@ object HttpResponseHandler {
     fun parseHttpException(e: Throwable): Exception {
         return if (e is HttpException) {
             try {
+                val json = Json { ignoreUnknownKeys = true }
                 val errorBody = e.response()?.errorBody()?.string()
-                val errorResponse = Gson().fromJson(errorBody, NullableApiResponse::class.java)
+                val errorResponse = errorBody?.let { json.decodeFromString<NullableApiResponse<Unit>>(it) }
+                    ?: NullableApiResponse(success = false, code = e.code(), message = e.message(), data = null)
                 when (errorResponse.code) {
                     in 3000..3999 -> HttpResponseException(errorResponse.code, "Redirection error : ${errorResponse.message}")
                     in 4000..4999 -> HttpResponseException(errorResponse.code, "Client error : ${errorResponse.message}")
