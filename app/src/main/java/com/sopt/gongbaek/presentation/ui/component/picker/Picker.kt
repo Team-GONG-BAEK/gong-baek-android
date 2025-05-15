8bb1279e9503bc7eb3bf9533d7e3fc3c29603167
wjdrjs00 <kimjk3409@gmail.com>
2025-05-15 16:47:32 +0900

    [chore] ktlintFormat 적용

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/component/picker/Picker.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/component/picker/Picker.kt
@@ -1,6 +1,5 @@
 package com.sopt.gongbaek.presentation.ui.component.picker
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
