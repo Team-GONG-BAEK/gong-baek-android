c8b75082600c1f9514bb27d998d7f734d2193fa5
wjdrjs00 <kimjk3409@gmail.com>
2025-06-09 15:10:34 +0900

    [Feat] 홈 화면 BackHandler 기능 구현
    - 한번 누른 경우 토스트 메시지 제공
    - 2초 이내 두번 누른 경우 앱 종료

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/home/screen/HomeScreen.kt
@@ -1,5 +1,8 @@
 package com.sopt.gongbaek.presentation.ui.home.screen
 
+import android.app.Activity
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -7,7 +10,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -23,6 +33,8 @@ import com.sopt.gongbaek.presentation.ui.home.component.section.OnceRecommendSec
 import com.sopt.gongbaek.presentation.ui.home.component.section.WeekRecommendSection
 import com.sopt.gongbaek.presentation.util.base.UiLoadState
 import com.sopt.gongbaek.ui.theme.GONGBAEKTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeRoute(
@@ -34,6 +46,25 @@ fun HomeRoute(
 ) {
     val uiState by viewModel.state.collectAsStateWithLifecycle()
     val lifecycleOwner = LocalLifecycleOwner.current
+
+    var backPressedOnce by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val activity = context as? Activity
+    val scope = rememberCoroutineScope()
+
+    BackHandler {
+        if (backPressedOnce) {
+            activity?.finish()
+        } else {
+            backPressedOnce = true
+            Toast.makeText(context, "뒤로 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+
+            scope.launch {
+                delay(2000L)
+                backPressedOnce = false
+            }
+        }
+    }
 
     LaunchedEffect(Unit) {
         viewModel.sideEffect
